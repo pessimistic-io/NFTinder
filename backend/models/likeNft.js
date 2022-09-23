43c073eb1ef48375c88e6c2f01042259769d740b
nikitaka7 <nikitaka7@mail.ru>
2022-09-23 20:14:12 +0700

    Models and Schema configuration

---
+++ backend/models/likeNft.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const likeSchema = new Schema({
+    nft: {
+        type: Schema.Types.ObjectId,
+        ref: 'Nft'
+    },
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    }
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model('LikeNft', likeSchema);
