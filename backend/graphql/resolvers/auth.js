5d3b22af491ce6c50929f57ea8b8aefacc55b4d8
nikitaka7 <nikitaka7@mail.ru>
2022-09-23 22:12:57 +0700

    auth request

---
+++ backend/graphql/resolvers/auth.js
@@ -0,0 +1,42 @@
+const Nft = require('../../models/nft');
+const User = require('../../models/user');
+
+//In progress
+module.exports = {
+  auth: async args => {
+    try {
+      const existingNft = await Nft.findOne({
+        chainId: args.nftInput.chainId,
+        collectionAddress: args.nftInput.collectionAddress,
+        tokenId: args.nftInput.tokenId,
+        ownerWallet: args.nftInput.ownerWallet,
+      })
+      const user = await User.findOne({
+        wallet: args.userInput.wallet
+      });
+      if (user != null && !existingNft) {
+        throw err;
+      }
+      if (existingNft && existingNft.ownerWallet == user.wallet) {
+        return existingNft;
+      } else {
+        const user = new User({
+          wallet: args.userInput.wallet,
+        });
+        const nft = new Nft({
+          chainId: args.nftInput.chainId,
+          collectionAddress: args.nftInput.collectionAddress,
+          tokenId: args.nftInput.tokenId,
+          ownerWallet: args.nftInput.ownerWallet,
+        });
+        if (user.wallet == nft.ownerWallet) {
+          await user.save();
+          await nft.save();
+          return nft;
+        }
+      }
+    } catch (err) {
+      throw err;
+    }
+  },
+};
