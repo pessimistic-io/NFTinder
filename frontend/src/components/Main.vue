e14b544823db157c0b2325b491d3a659ba4ec0c9
vladimir <vladimir@pessimistic.io>
2022-09-24 18:22:44 +0300

    + nft submit button

--- frontend/src/components/Main.vue
+++ frontend/src/components/Main.vue
@@ -1,15 +1,17 @@
 <template>
   <div :class="$style.Main">
-    <h3>Your account: {{ target_account }}</h3>
+    <h3>Your account: {{ main_account }}</h3>
     <form>
       <h4>Select the NFT that you want to swap</h4>
       <p v-if="nfts.length === 0">You have no NFTs in your wallet</p>
       <ul v-else>
         <li v-for="nft in nfts">
-          <input :id="nft.id" type="radio">
+          <input :id="nft.id" name="nft" type="radio" v:onclick="">
           <label :for="nft.id">{{ nft.name }}</label>
         </li>
       </ul>
+
+      <button v:onclick="submit">Use this nfts for play</button>
     </form>
   </div>
 </template>
@@ -30,6 +32,16 @@ export default {
       target_account: "0xab5801a7d398351b8be11c439e05c5b3259aec9b",
       nfts: Array(0),
       quicknode_provider: {},
+      selected_nft: {}
+      /* //graphql format:
+        type Nft{
+          _id: ID!
+          chainId: String!
+          collectionAddress: String!
+          tokenId: String!
+          ownerWallet: String!
+        }
+      */
     }
   },
 
@@ -67,11 +79,26 @@ export default {
       }
 
       const nfts = await this.quicknode_provider.send("qn_fetchNFTs", {
-        wallet: this.target_account
+        wallet: this.target_account,
+        page: 1,
+        perPage: 40
       });
       console.log(nfts);
       this.nfts = nfts.assets;
     },
+
+    async submit() {
+
+      const response = await fetch('/graphql', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(this.selected_nft)
+      });
+
+      console.log(response)
+    }
   },
 };
 
@@ -79,6 +106,6 @@ export default {
 <style lang="scss" module>
 
 .Main {
-  //
+  maring:2em;
 }
 </style>
